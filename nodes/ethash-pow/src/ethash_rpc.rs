233dfd3c262a9614148cb65ec77d5e9ca21721e4
tristan <tristan.lee@qq.com>
2021-05-31 20:55:13 +0800

    add cmd for eth_getwork()

--- nodes/ethash-pow/src/ethash_rpc.rs
+++ nodes/ethash-pow/src/ethash_rpc.rs
@@ -1,25 +1,37 @@
 
 use jsonrpc_core::Result;
+use jsonrpc_core::Error;
 use jsonrpc_derive::rpc;
-use runtime::{self, opaque::Block, RuntimeApi};
-use futures::channel::mpsc::Sender;
+
+use futures::{
+	channel::{mpsc, oneshot},
+	TryFutureExt,
+	FutureExt,
+	SinkExt
+};
+
+// use futures::channel::mpsc::Sender;
 // use sc_consensus_pow::{MiningWorker, MiningMetadata, MiningBuild};
 // use sc_consensus_pow::{Error, PowAlgorithm};
 // use sp_runtime::traits::{Block as BlockT, Header as HeaderT};
 // use sp_api::ProvideRuntimeApi;
 // use parking_lot::Mutex;
+use runtime::{self, opaque::Block, RuntimeApi};
 use std::sync::Arc;
 use ethereum_types::{H160, H256, H64, U256, U64};
 use crate::types::work::{Work};
 use crate::helpers::{errors};
 
+/// Future's type for jsonrpc
+type FutureResult<T> = Box<dyn jsonrpc_core::futures::Future<Item = T, Error = Error> + Send>;
+/// sender passed to the authorship task to report errors or successes.
+pub type Sender<T> = Option<oneshot::Sender<std::result::Result<T, String>>>;
+
 /// Message sent to the background authorship task, usually by RPC.
 pub enum EtheminerCmd<Hash> {
 	GetWork {
-		/// specify the parent hash of the about-to-created block
-		parent_hash: Option<Hash>,
 		/// sender to report errors/success to the rpc.
-		sender: Sender<()>,
+		sender: Sender<Work>,
 	},
 	/// Tells the engine to finalize the block with the supplied hash
 	SubmitWork {
@@ -33,7 +45,7 @@ pub enum EtheminerCmd<Hash> {
 #[rpc(server)]
 pub trait EthashRpc {
 	#[rpc(name = "eth_getWork")]
-    fn eth_getWork(&self, _: Option<u64>) -> Result<Work>;
+    fn eth_getWork(&self, _: Option<u64>) -> FutureResult<Work>;
 
 	#[rpc(name = "eth_submitWork")]
 	fn eth_submitWork(&self, _: H64, _: H256, _: H256) -> Result<bool>;
@@ -62,8 +74,18 @@ impl<C, Hash> EthashData<C, Hash> {
 }
 
 impl<C: Send + Sync + 'static, Hash: Send + 'static> EthashRpc for EthashData<C, Hash> {
-	fn eth_getWork(&self, no_new_work_timeout: Option<u64>) -> Result<Work> {
-		return Err(errors::no_work());
+	fn eth_getWork(&self, no_new_work_timeout: Option<u64>) -> FutureResult<Work> {
+		let mut sink = self.command_sink.clone();
+		let future = async move {
+			let (sender, receiver) = oneshot::channel();
+			let command = EtheminerCmd::GetWork {
+				sender: Some(sender),
+			};
+			sink.send(command).await?;
+			receiver.await?
+		}.boxed();
+
+		Box::new(future.map_err(Error::from).compat())
 	}
 
 	fn eth_submitWork(&self, _: H64, _: H256, _: H256) -> Result<bool> {
